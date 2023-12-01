72d0c8b3e8953ed1fb95383e9156630b407d04b7
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2023-12-01 02:08:38 -0300

    Add day 1 part 1 solution

--- Day01/Solution.cs
+++ Day01/Solution.cs
@@ -2,9 +2,18 @@
 {
     internal class Solution : ISolution
     {
-        public static object? RunPart1()
+        public static object RunPart1()
         {
-            return null;
+            var lines = File.ReadAllLines("Day01\\Input.txt");
+            var sum = 0;
+            foreach (var line in lines)
+            {
+                var digits = line.Where(char.IsDigit);
+                var number = int.Parse($"{digits.First()}{digits.Last()}");
+                sum += number;
+            }
+
+            return sum;
         }
 
         public static object? RunPart2()
