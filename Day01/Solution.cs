a31223f27b22844e905a408ac0132c37ad3e0b16
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2023-12-01 03:22:09 -0300

    Add day 1 part 2 solution

--- Day01/Solution.cs
+++ Day01/Solution.cs
@@ -2,9 +2,10 @@
 {
     internal class Solution : ISolution
     {
+        private static readonly string[] lines = File.ReadAllLines("Day01\\Input.txt");
+
         public static object RunPart1()
         {
-            var lines = File.ReadAllLines("Day01\\Input.txt");
             var sum = 0;
             foreach (var line in lines)
             {
@@ -16,9 +17,111 @@
             return sum;
         }
 
-        public static object? RunPart2()
+        public static object RunPart2()
         {
-            return null;
+            var sum = 0;
+            foreach (var line in lines)
+            {
+                var number = GetNumber(line);
+                sum += number;
+            }
+
+            return sum;
+        }
+
+        private static int GetNumber(string line)
+        {
+            const int MaxLength = 5; // "three", "seven" and "eight"
+            const int MinLength = 1; // a digit
+
+            string? firstDigit = null;
+
+            var i = 0;
+            var length = MinLength;
+            while (i < line.Length && firstDigit is null)
+            {
+                if (char.IsDigit(line[i]))
+                {
+                    firstDigit = line[i].ToString();
+                    break;
+                }
+
+                if (i + length > line.Length)
+                {
+                    length = MinLength;
+                    i++;
+                }
+
+                var word = line.Substring(i, length);
+                var wordAsDigit = SwapWordForDigit(word);
+
+                if (word != wordAsDigit)
+                {
+                    firstDigit = wordAsDigit;
+                }
+
+                if (length == MaxLength)
+                {
+                    length = MinLength;
+                    i++;
+                }
+                else
+                {
+                    length++;
+                }
+            }
+
+            string? lastDigit = null;
+            i = line.Length - MinLength;
+            length = MinLength;
+            while (i >= 0 && lastDigit is null)
+            {
+                if (char.IsDigit(line[i]))
+                {
+                    lastDigit = line[i].ToString();
+                    break;
+                }
+
+                if (i + length > line.Length)
+                {
+                    length = MinLength;
+                    i--;
+                }
+
+                var word = line.Substring(i, length);
+                var wordAsDigit = SwapWordForDigit(word);
+
+                if (word != wordAsDigit)
+                {
+                    lastDigit = wordAsDigit;
+                }
+
+                if (length == MaxLength)
+                {
+                    length = MinLength;
+                    i--;
+                }
+                else
+                {
+                    length++;
+                }
+            }
+
+            return int.Parse($"{firstDigit}{lastDigit}");
+
+            static string SwapWordForDigit(string word) => word switch
+            {
+                "one" => "1",
+                "two" => "2",
+                "three" => "3",
+                "four" => "4",
+                "five" => "5",
+                "six" => "6",
+                "seven" => "7",
+                "eight" => "8",
+                "nine" => "9",
+                _ => word
+            };
         }
     }
 }
