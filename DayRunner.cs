b49f4d17e2a753a668861c665d035667ed4d3f46
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2023-12-08 03:34:36 -0300

    Add elapsed time to DayRunner

--- DayRunner.cs
+++ DayRunner.cs
@@ -1,25 +1,30 @@
-﻿namespace AdventOfCode2023
+﻿using System.Diagnostics;
+
+namespace AdventOfCode2023
 {
     internal static class DayRunner<TDay> where TDay : ISolution
     {
-
         public static void Run()
         {
+            var stopwatch = new Stopwatch();
             Console.WriteLine("========== Part 1 ==========");
+
+            stopwatch.Start();
             var part1Solution = TDay.RunPart1();
-            if (part1Solution is not null)
-            {
-                Console.WriteLine(part1Solution.ToString());
-            }
+            stopwatch.Stop();
+
+            Console.WriteLine(part1Solution?.ToString());
+            Console.WriteLine($"Elapsed time: {stopwatch.Elapsed}");
 
             Console.WriteLine();
-
             Console.WriteLine("========== Part 2 ==========");
+
+            stopwatch.Restart();
             var part2Solution = TDay.RunPart2();
-            if (part2Solution is not null)
-            {
-                Console.WriteLine(part2Solution.ToString());
-            }
+            stopwatch.Stop();
+
+            Console.WriteLine(part2Solution?.ToString());
+            Console.WriteLine($"Elapsed time: {stopwatch.Elapsed}");
         }
     }
 }
