7ffb73c7cdf4566ab28d145b1477269cc7840c45
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2023-12-08 03:18:30 -0300

    Add day 8 part 2 solution

--- Day08/Solution.cs
+++ Day08/Solution.cs
@@ -1,14 +1,53 @@
-﻿namespace AdventOfCode2023.Day08
+﻿using MathNet.Numerics;
+
+namespace AdventOfCode2023.Day08
 {
     internal class Solution : ISolution
     {
+        private enum Instruction
+        {
+            Left,
+            Right
+        }
+
         private readonly record struct Node(string Label, string LeftNodeLabel, string RightNodeLabel);
         private static readonly string[] lines = File.ReadAllLines("Day08\\Input.txt");
 
         public static object RunPart1()
         {
-            var instructions = lines[0].ToCharArray();
-            var nodes = lines[2..]
+            var instructions = ParseInstructions();
+            var nodes = ParseNodes();
+
+            var instructionIndex = 0;
+            var node = GetNodesEndingWith(nodes, "AAA").Single();
+            var steps = 0;
+
+            while (node.Label != "ZZZ")
+            {
+                var instruction = instructions[instructionIndex++];
+                if (instructionIndex >= instructions.Count)
+                {
+                    instructionIndex = 0;
+                }
+
+                node = GetNextNode(nodes, node, instruction);
+                steps++;
+            }
+
+            return steps;
+        }
+
+        private static List<Instruction> ParseInstructions()
+        {
+            return lines[0]
+                .ToCharArray()
+                .Select(i => i == 'L' ? Instruction.Left : Instruction.Right)
+                .ToList();
+        }
+
+        private static List<Node> ParseNodes()
+        {
+            return lines[2..]
                 .Select(n =>
                 {
                     var parts = n.Split(" = ");
@@ -19,36 +58,70 @@
                     return new Node(label, left, right);
                 })
                 .ToList();
+        }
 
-            var instructionIndex = 0;
-            Node? node = null;
-            for (var i = 0; i < nodes.Count || node is null; i++)
+        private static IEnumerable<Node> GetNodesEndingWith(List<Node> nodes, string pattern)
+        {
+            for (var i = 0; i < nodes.Count; i++)
             {
-                if (nodes[i].Label == "AAA")
+                if (nodes[i].Label.EndsWith(pattern))
                 {
-                    node = nodes[i];
+                    yield return nodes[i];
                 }
             }
+        }
 
-            var steps = 0;
-            while (node!.Value.Label != "ZZZ")
-            {
-                var instruction = instructions[instructionIndex++];
-                if (instructionIndex >= instructions.Length)
-                {
-                    instructionIndex = 0;
-                }
+        private static IEnumerable<Node> GetNodesEndingWith(List<Node> nodes, char endingCharacter)
+        {
+            return GetNodesEndingWith(nodes, endingCharacter.ToString());
+        }
 
-                node = nodes.Single(n => n.Label == (instruction == 'L' ? node!.Value.LeftNodeLabel : node!.Value.RightNodeLabel));
-                steps++;
-            }
+        private static Node GetNextNode(List<Node> nodes, Node currentNode, Instruction instruction)
+        {
+            var nextLabel = instruction == Instruction.Left
+                ? currentNode.LeftNodeLabel
+                : currentNode.RightNodeLabel;
 
-            return steps;
+            return nodes.Single(n => n.Label == nextLabel);
         }
 
         public static object RunPart2()
         {
-            return null;
+            var instructions = ParseInstructions();
+            var allNodes = ParseNodes();
+            var nodesEndingWithZ = GetNodesEndingWith(allNodes, 'Z').ToList();
+            var nodes = GetNodesEndingWith(allNodes, 'A').ToList();
+
+            var instructionIndex = 0;
+            var steps = 0L;
+            var nodeCycles = new (long Length, bool FinishedCalculating)[nodes.Count];
+
+            while (!nodeCycles.All(n => n.FinishedCalculating))
+            {
+                var instruction = instructions[instructionIndex++];
+                if (instructionIndex >= instructions.Count)
+                {
+                    instructionIndex = 0;
+                }
+
+                for (var i = 0; i < nodes.Count; i++)
+                {
+                    nodes[i] = GetNextNode(allNodes, nodes[i], instruction);
+
+                    if (!nodeCycles[i].FinishedCalculating)
+                    {
+                        nodeCycles[i].Length++;
+                    }
+
+                    if (nodes[i].Label.EndsWith('Z'))
+                    {
+                        nodeCycles[i].FinishedCalculating = true;
+                    }
+                }
+                steps++;
+            }
+
+            return Euclid.LeastCommonMultiple(nodeCycles.Select(n => n.Length).ToList());
         }
     }
 }
