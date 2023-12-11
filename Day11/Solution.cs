0521fbb68438b952b0aba6ea1fa81db8c47c3ead
Fernando Fonseca <54486077+FernandoWF@users.noreply.github.com>
2023-12-11 04:53:15 -0300

    Add day 11 part 2 solution

--- Day11/Solution.cs
+++ Day11/Solution.cs
@@ -9,7 +9,7 @@
             var jaggedUniverse = lines
                 .Select(l => l.ToCharArray())
                 .ToArray();
-            var universe = To2dArray(jaggedUniverse);
+            var universe = ConvertTo2dArray(jaggedUniverse);
             var expandedUniverse = ExpandUniverse(universe);
 
             var galaxies = new List<(int X, int Y)>();
@@ -43,7 +43,7 @@
             return shortestPathLengthSum;
         }
 
-        public static T[,] To2dArray<T>(T[][] jaggedArray)
+        public static T[,] ConvertTo2dArray<T>(T[][] jaggedArray)
         {
             var firstInnerArrayLength = jaggedArray[0].Length;
             var array = new T[firstInnerArrayLength, jaggedArray.Length];
@@ -66,6 +66,47 @@
         }
 
         private static char[,] ExpandUniverse(char[,] universe)
+        {
+            (var emptyRowIndexes, var emptyColumnIndexes) = GetEmptyIndexes(universe);
+            var rowCount = universe.GetLength(1);
+            var columnCount = universe.GetLength(0);
+            var newColumnCount = columnCount + emptyColumnIndexes.Count;
+            var newRowCount = rowCount + emptyRowIndexes.Count;
+            var resizedUniverse = new char[newColumnCount, newRowCount];
+
+            for (int y = 0, originalUniverseY = 0; y < newRowCount; y++, originalUniverseY++)
+            {
+                if (emptyRowIndexes.Contains(originalUniverseY))
+                {
+                    for (var x = 0; x < newColumnCount; x++)
+                    {
+                        resizedUniverse[x, y] = '.';
+                        resizedUniverse[x, y + 1] = '.';
+                    }
+                    y++;
+                }
+                else
+                {
+                    for (int x = 0, originalUniverseX = 0; x < newColumnCount; x++, originalUniverseX++)
+                    {
+                        if (emptyColumnIndexes.Contains(originalUniverseX))
+                        {
+                            resizedUniverse[x, y] = '.';
+                            resizedUniverse[x + 1, y] = '.';
+                            x++;
+                        }
+                        else
+                        {
+                            resizedUniverse[x, y] = universe[originalUniverseX, originalUniverseY];
+                        }
+                    }
+                }
+            }
+
+            return resizedUniverse;
+        }
+
+        private static (HashSet<int> emptyRowIndexes, HashSet<int> emptyColumnIndexes) GetEmptyIndexes(char[,] universe)
         {
             var emptyRowIndexes = new HashSet<int>();
             var rowCount = universe.GetLength(1);
@@ -107,45 +148,70 @@
                 }
             }
 
-            var newColumnCount = columnCount + emptyColumnIndexes.Count;
-            var newRowCount = rowCount + emptyRowIndexes.Count;
-            var resizedUniverse = new char[newColumnCount, newRowCount];
-
-            for (int y = 0, originalUniverseY = 0; y < newRowCount; y++, originalUniverseY++)
-            {
-                if (emptyRowIndexes.Contains(originalUniverseY))
-                {
-                    for (var x = 0; x < newColumnCount; x++)
-                    {
-                        resizedUniverse[x, y] = '.';
-                        resizedUniverse[x, y + 1] = '.';
-                    }
-                    y++;
-                }
-                else
-                {
-                    for (int x = 0, originalUniverseX = 0; x < newColumnCount; x++, originalUniverseX++)
-                    {
-                        if (emptyColumnIndexes.Contains(originalUniverseX))
-                        {
-                            resizedUniverse[x, y] = '.';
-                            resizedUniverse[x + 1, y] = '.';
-                            x++;
-                        }
-                        else
-                        {
-                            resizedUniverse[x, y] = universe[originalUniverseX, originalUniverseY];
-                        }
-                    }
-                }
-            }
-
-            return resizedUniverse;
+            return (emptyRowIndexes, emptyColumnIndexes);
         }
 
         public static object RunPart2()
         {
-            return null;
+            const int ExpandedRowOrColumnSize = 1000000;
+            const int ExpansionSize = ExpandedRowOrColumnSize - 1;
+            var jaggedUniverse = lines
+                .Select(l => l.ToCharArray())
+                .ToArray();
+            var universe = ConvertTo2dArray(jaggedUniverse);
+            (var emptyRowIndexes, var emptyColumnIndexes) = GetEmptyIndexes(universe);
+
+            var galaxies = new List<(int X, int Y)>();
+            var columnCount = universe.GetLength(0);
+            var rowCount = universe.GetLength(1);
+
+            for (var y = 0; y < rowCount; y++)
+            {
+                for (var x = 0; x < columnCount; x++)
+                {
+                    if (universe[x, y] == '#')
+                    {
+                        galaxies.Add((x, y));
+                    }
+                }
+            }
+
+            var shortestPathLengthSum = 0L;
+            for (var galaxy1Index = 0; galaxy1Index < galaxies.Count; galaxy1Index++)
+            {
+                for (var galaxy2Index = galaxy1Index + 1; galaxy2Index < galaxies.Count; galaxy2Index++)
+                {
+                    var (galaxy1X, galaxy1Y) = galaxies[galaxy1Index];
+                    var (galaxy2X, galaxy2Y) = galaxies[galaxy2Index];
+
+                    var minX = Math.Min(galaxy1X, galaxy2X);
+                    var maxX = Math.Max(galaxy1X, galaxy2X);
+                    var minY = Math.Min(galaxy1Y, galaxy2Y);
+                    var maxY = Math.Max(galaxy1Y, galaxy2Y);
+
+                    var shortestPathHorizontalDistance = maxX - minX;
+                    for (var x = minX + 1; x < maxX; x++)
+                    {
+                        if (emptyColumnIndexes.Contains(x))
+                        {
+                            shortestPathHorizontalDistance += ExpansionSize;
+                        }
+                    }
+
+                    var shortestPathVerticalDistance = maxY - minY;
+                    for (var y = minY + 1; y < maxY; y++)
+                    {
+                        if (emptyRowIndexes.Contains(y))
+                        {
+                            shortestPathVerticalDistance += ExpansionSize;
+                        }
+                    }
+
+                    shortestPathLengthSum += shortestPathHorizontalDistance + shortestPathVerticalDistance;
+                }
+            }
+
+            return shortestPathLengthSum;
         }
     }
 }
